5368cd840770a1f842cc1207e086d32d8342be36
Hugo Alliaume <kocal@live.fr>
2017-04-06 15:40:58 +0200

    RUSH MASSIF DEMO POLL

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,9 +15,6 @@ class DefaultController extends Controller
      */
     public function indexAction(/** @noinspection PhpUnusedParameterInspection */  Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('@App/index.html.twig');
     }
 }
