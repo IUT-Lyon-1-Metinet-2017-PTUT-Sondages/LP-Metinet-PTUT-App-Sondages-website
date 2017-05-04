b16453ae4e7de27e78a654df643d397da4ec8839
Hugo Alliaume <kocal@live.fr>
2017-05-04 17:28:34 +0200

    oui

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -2,17 +2,32 @@
 
 namespace AppBundle\Controller;
 
+use FOS\UserBundle\Model\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends Controller
 {
     /**
      * @Route("/backoffice/users", name="backoffice_users")
      * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request) {
-        die('UsersController');
+    public function indexAction(Request $request)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $paginator = $this->get('knp_paginator');
+
+        $pagination = $paginator->paginate(
+            $userManager->findUsers(),
+            $request->query->getInt('page', 1),
+            20
+        );
+
+        return $this->render('@App/backoffice/users/index.html.twig', [
+            'pagination' => $pagination
+        ]);
     }
 }
