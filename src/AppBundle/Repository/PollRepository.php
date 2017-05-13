1c82834bf195742676974dd6c7f1bc387cbdaa9b
Anthony Margerand <anthony.margerand@gmail.com>
2017-05-13 12:15:44 +0200

    fix merge trouble on poll results

--- src/AppBundle/Repository/PollRepository.php
+++ src/AppBundle/Repository/PollRepository.php
@@ -13,19 +13,15 @@ class PollRepository extends EntityRepository
     public function findResultsFromPoll($id)
     {
         return $this->createQueryBuilder('p')
-                    ->select('q.id', 'q.title', 'pr.id', 'pr.title', 'COUNT(pr.id) as amount')
-                    ->innerJoin('p.questions', 'q')
-                    ->innerJoin('q.propositions', 'pr')
-                    ->innerJoin('pr.answers', 'a')
-                    ->groupBy('pr.id')
-                    ->where('p.id = :id')
-                    ->setParameter('id', $id)
-                    ->getQuery()
-                    ->getResult();
+            ->select('q.id as qId', 'q.title as qTitle', 'pr.id as propId', 'pr.title as propTitle', 'COUNT(pr.id) as amount')
+            ->innerJoin('p.questions', 'q')
+            ->innerJoin('q.propositions', 'pr')
+            ->innerJoin('pr.answers', 'a')
+            ->groupBy('pr.id')
+            ->where('p.id = :id')
+            ->setParameter('id', $id)
+            ->getQuery()->getResult();
     }
-
-
-
 
     public function findFullObjectById($id)
     {
